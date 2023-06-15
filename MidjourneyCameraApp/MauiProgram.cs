6d959a1e1e096de277b74b0ccb0045e288ce0c51
Ricardo R <rretamal.dev@gmail.com>
2023-06-15 10:52:24 -0400

    Add font awesome

--- MidjourneyCameraApp/MauiProgram.cs
+++ MidjourneyCameraApp/MauiProgram.cs
@@ -15,7 +15,8 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+                fonts.AddFont("Font-Awesome-6-Free-Solid-900.otf", "FASolid");
+            });
 
 #if DEBUG
 		builder.Logging.AddDebug();
